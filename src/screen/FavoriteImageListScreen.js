cb9299317c6b1741be4d5ca675b3e290e8a19a59
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-26 14:21:28 +0900

    run "create-expo-app" and project&components setting

---
+++ src/screen/FavoriteImageListScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Typography } from "../components/Typography";
+
+export default function FavoriteImageListScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Typography fontSize={20}>FavoriteImageListScreen</Typography>
+    </View>
+  );
+}
---
+++ src/screen/ImageDetailScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Typography } from "../components/Typography";
+
+export default function ImageDetailScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Typography fontSize={20}>ImageDetailScreen</Typography>
+    </View>
+  );
+}
