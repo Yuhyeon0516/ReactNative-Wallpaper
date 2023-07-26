3ebe7c52716a335c5f3bcf151235884bfadfb1b5
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-26 15:08:59 +0900

    crate ImageDetailScreen

--- src/components/PhotoListItem.js
+++ src/components/PhotoListItem.js
@@ -2,11 +2,15 @@ import { useWindowDimensions } from "react-native";
 import { CustomButton } from "./CustomButton";
 import { RemoteImage } from "./RemoteImage";
 import { useCallback } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function PhotoListItem({ url }) {
   const { width } = useWindowDimensions();
+  const navigation = useNavigation();
 
-  const onPressItem = useCallback(() => {});
+  const onPressItem = useCallback(() => {
+    navigation.navigate("ImageDetail", { url: url.item });
+  }, []);
 
   return (
     <CustomButton onPress={onPressItem} paddingH={20} paddingV={10}>
--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -1,10 +1,31 @@
-import { View } from "react-native";
+import { View, useWindowDimensions } from "react-native";
 import { Typography } from "../components/Typography";
+import { Header } from "../components/Header/Header";
+import { useCallback } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { RemoteImage } from "../components/RemoteImage";
 
 export default function ImageDetailScreen() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const onPressBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
+
+  const { width } = useWindowDimensions();
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Typography fontSize={20}>ImageDetailScreen</Typography>
+    <View style={{ flex: 1 }}>
+      <Header>
+        <Header.Group>
+          <Header.Icon iconName={"arrow-back"} onPress={onPressBack} />
+          <Header.Title title={"Image Detail"} />
+        </Header.Group>
+      </Header>
+
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <RemoteImage url={route.params.url} width={width - 10} height={width * 1.5} />
+      </View>
     </View>
   );
 }
