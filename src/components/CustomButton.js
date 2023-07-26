7424fc182b44185b34d6255235e7941b849b869c
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-26 15:57:05 +0900

    add animation if image click

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Pressable } from "react-native";
 
-export function CustomButton({ onPress, hitSlop, paddingV, paddingH, children }) {
+export function CustomButton({ onPressIn, onPressOut, onPress, hitSlop, paddingV, paddingH, children }) {
   return (
     <Pressable
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
       onPress={onPress}
       hitSlop={hitSlop ?? { left: 0, right: 0, top: 0, bottom: 0 }}
       style={{ paddingHorizontal: paddingH, paddingVertical: paddingV }}
