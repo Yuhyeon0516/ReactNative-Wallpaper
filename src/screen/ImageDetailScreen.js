c6f952f5bfc0d05791d685666302f32b11669b8c
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-26 15:57:14 +0900

    add download indicator

--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -1,7 +1,7 @@
-import { View, useWindowDimensions } from "react-native";
+import { ActivityIndicator, View, useWindowDimensions } from "react-native";
 import { Typography } from "../components/Typography";
 import { Header } from "../components/Header/Header";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { RemoteImage } from "../components/RemoteImage";
 import { CustomButton } from "../components/CustomButton";
@@ -12,20 +12,19 @@ import * as MediaLibrary from "expo-media-library";
 export default function ImageDetailScreen() {
   const navigation = useNavigation();
   const route = useRoute();
+  const [downloading, setDownloading] = useState(false);
   const onPressBack = useCallback(() => {
     navigation.goBack();
   }, []);
 
   const onPressDownload = useCallback(async () => {
+    setDownloading(true);
     const downloadResumable = FileSystem.createDownloadResumable(route.params.url, `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`);
 
     try {
       const { uri } = await downloadResumable.downloadAsync();
-      console.log(`Finished download ${uri}`);
 
       const permissionResult = await MediaLibrary.getPermissionsAsync(true);
-      console.log(permissionResult);
-
       if (permissionResult.status === "denied") {
         return;
       }
@@ -38,11 +37,11 @@ export default function ImageDetailScreen() {
       }
 
       const asset = await MediaLibrary.createAssetAsync(uri);
-      const album = await MediaLibrary.createAlbumAsync("MyAlbum", asset, false);
-
-      console.log(album);
+      await MediaLibrary.createAlbumAsync("MyAlbum", asset, false);
     } catch (error) {
       console.log(error);
+    } finally {
+      setDownloading(false);
     }
   }, []);
 
@@ -58,14 +57,20 @@ export default function ImageDetailScreen() {
       </Header>
 
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <RemoteImage url={route.params.url} width={width - 10} height={width * 1.5} />
+        <RemoteImage url={route.params.url} width={width - 10} height={width * 1.2} />
       </View>
       <CustomButton onPress={onPressDownload}>
         <View style={{ paddingBottom: 24, backgroundColor: "black" }}>
-          <View style={{ height: 52, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
-            <Typography color={"white"}>Download</Typography>
-            <Icon iconName={"download"} size={24} color={"white"} />
-          </View>
+          {downloading ? (
+            <View style={{ height: 52, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+              <ActivityIndicator />
+            </View>
+          ) : (
+            <View style={{ height: 52, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+              <Typography color={"white"}>Download</Typography>
+              <Icon iconName={"download"} size={24} color={"white"} />
+            </View>
+          )}
         </View>
       </CustomButton>
     </View>
