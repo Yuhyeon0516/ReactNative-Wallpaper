caaff5c9e73a1a10ca165bd74ad18d1d71b5888b
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-26 14:39:31 +0900

    create stack navigation

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import BottomTabNavigations from "./src/navigations/BottomTabNavigations";
+import RootStackNavigations from "./src/navigations/RootStackNavigations";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTabNavigations />
+      <RootStackNavigations />
     </NavigationContainer>
   );
 }
